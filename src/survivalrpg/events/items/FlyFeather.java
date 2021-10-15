d58fb8414818443849d5d06c9fdb5286cacf0485
danizml <josedanielmorebolopez@gmail.com>
2021-10-15 19:13:24 +0200

    Changes by TonimatasMC

--- src/survivalrpg/events/items/FlyFeather.java
+++ src/survivalrpg/events/items/FlyFeather.java
@@ -9,7 +9,7 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import survivalrpg.SurvivalRPG;
 
-@SuppressWarnings({"deprecation", "EqualsBetweenInconvertibleTypes"})
+@SuppressWarnings({"deprecation", "EqualsBetweenInconvertibleTypes", "ConstantConditions", "PointlessBooleanExpression"})
 public class FlyFeather implements Listener {
     private final SurvivalRPG plugin;
 
@@ -19,16 +19,23 @@ public class FlyFeather implements Listener {
 
     @EventHandler
     public void onClickFeather(PlayerInteractEvent event) {
-        final Player player = event.getPlayer();
-        if ((event.getAction().equals(Action.RIGHT_CLICK_AIR)) | (event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) | (event.getAction().equals(Action.LEFT_CLICK_AIR)) | (event.getAction().equals(Action.LEFT_CLICK_BLOCK))) {
+        Player player = event.getPlayer();
+        if (event.getAction().equals(Action.RIGHT_CLICK_AIR) || event.getAction().equals(Action.RIGHT_CLICK_BLOCK) || event.getAction().equals(Action.LEFT_CLICK_AIR) || event.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
             if (player.getItemInHand().equals(Material.FEATHER)) {
-                if (Objects.requireNonNull(player.getItemInHand().getItemMeta()).getDisplayName().equals(this.plugin.getItems().getString("Fly-Feather.name"))) {
-                    if (Objects.equals(player.getItemInHand().getItemMeta().getLore(), this.plugin.getItems().getStringList("Fly-Feather.lore"))) {
-                        player.sendMessage(this.plugin.name + ChatColor.DARK_GREEN + " " + this.plugin.getItems().getString("Fly-Feather.message"));
-                        player.setAllowFlight(true);
-                        player.getItemInHand().setType(Material.PAPER);
-                        player.sendMessage(this.plugin.name + ChatColor.DARK_GREEN + "You has been fly disabled");
+                if (player.getItemInHand().getItemMeta().getDisplayName().equals(ChatColor.translateAlternateColorCodes('&', this.plugin.getItems().getString("Fly-Feather.name")))) {
+                    player.sendMessage(this.plugin.name + ChatColor.DARK_GREEN + " " + this.plugin.getItems().getString("Fly-Feather.message"));
+
+                    if (player.getAllowFlight() == true) {
+                        player.setAllowFlight(false);
+
                     }
+
+                    if (player.getAllowFlight() == false) {
+                        player.setAllowFlight(true);
+                        player.sendMessage(this.plugin.name + ChatColor.DARK_GREEN + "You has been fly enabled");
+                    }
+
+                    player.getItemInHand().setType(Material.PAPER);
                 }
             }
         }
