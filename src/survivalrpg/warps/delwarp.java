f8de8d4a4de2cb465dbb0b03b68bedbcae2552e5
danizml <josedanielmorebolopez@gmail.com>
2021-10-16 11:42:08 +0200

    Updated: delwarp.java

--- src/survivalrpg/warps/delwarp.java
+++ src/survivalrpg/warps/delwarp.java
@@ -21,26 +21,26 @@ public class delwarp implements CommandExecutor {
     @Override
     public boolean onCommand(@Nonnull CommandSender sender, @Nonnull Command command, @Nonnull String s, @Nonnull String[] strings) {
         if (!(sender instanceof Player)) {
-            sender.sendMessage(ChatColor.RED + "[rpg] Player only command!");
+            sender.sendMessage(ChatColor.RED + "[RPG] Player only command!");
         } else {
             if (sender.hasPermission("survivalrpg.warp.delwarp")) {
                 Player player = (Player) sender;
 
                 if (strings.length == 0) {
-                    player.sendMessage(ChatColor.BLUE + ("[rpg] Please provide a name!"));
+                    player.sendMessage(ChatColor.BLUE + ("[RPG] Please provide a name!"));
                 }
 
                 String name = strings[0].toLowerCase();
                 if (plugin.getConfig().get(name) == null) {
-                    player.sendMessage(ChatColor.RED + ("[rpg] There is no warp with this name!"));
+                    player.sendMessage(ChatColor.RED + ("[RPG] There is no warp with this name!"));
                 }
 
                 plugin.getConfig().set(name, null);
                 plugin.saveConfig();
-                player.sendMessage(ChatColor.GREEN + ("[rpg] Warp" + name + "asuccessfully deleted!"));
+                player.sendMessage(ChatColor.GREEN + ("[RPG] Warp" + name + "asuccessfully deleted!"));
 
             } else {
-                sender.sendMessage(ChatColor.RED + ("[rpg] You don't have enough permissions!"));
+                sender.sendMessage(ChatColor.RED + ("[RPG] You don't have enough permissions!"));
             }
         }return true;
     }
