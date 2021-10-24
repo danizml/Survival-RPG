59cc60ff05b8feb0853cf44a334294925dabe1d3
danizml <josedanielmorebolopez@gmail.com>
2021-10-25 01:01:32 +0200

    Added: RegisterManager.java

---
+++ src/survivalrpg/manager/RegisterManager.java
@@ -0,0 +1,67 @@
+package survivalrpg.manager;
+
+import org.bukkit.plugin.PluginManager;
+import survivalrpg.SurvivalRPG;
+import survivalrpg.commands.PrimaryCommand;
+import survivalrpg.events.items.FlyFeather;
+import survivalrpg.events.items.TeleportBow;
+import survivalrpg.menus.HelpMenu;
+import survivalrpg.menus.PrimaryMenu;
+import survivalrpg.storage.TabulatorCompleter;
+import survivalrpg.warps.delwarp;
+import survivalrpg.warps.setwarp;
+import survivalrpg.warps.warp;
+
+import java.util.Objects;
+
+/**
+ *    +RegisterManager+
+ * File created by TonimatasMC
+ * Links:
+ *  - Discord: https://discord.com/invite/VYqEtT36U9
+ *  - GitHub: https://github.com/TonimatasMCDEV
+ *  - SpigotMC: https://www.spigotmc.org/members/tonimatas.803111/
+ *  - CurseForge: https://www.curseforge.com/members/x_tonimatasmc_x/projects
+ *
+ */
+
+public class RegisterManager {
+
+
+    public static void register() {
+        registerYML();
+        registerCommands();
+        registerEvents();
+        registerMenus();
+        MessageManager.register();
+        MessageManager.startPlugin();
+    }
+
+    public static void registerYML() {
+        SurvivalRPG.getInstance().registerConfig();
+        SurvivalRPG.getInstance().registerWarps();
+        SurvivalRPG.getInstance().registerMessages();
+        SurvivalRPG.getInstance().registerItems();
+    }
+
+    public static void registerCommands() {
+        Objects.requireNonNull(SurvivalRPG.getInstance().getCommand("rpgs")).setExecutor(new EconomyManager());
+        Objects.requireNonNull(SurvivalRPG.getInstance().getCommand("rpg")).setExecutor(new PrimaryCommand(SurvivalRPG.getInstance()));
+        Objects.requireNonNull(SurvivalRPG.getInstance().getCommand("rpg")).setTabCompleter(new TabulatorCompleter());
+        Objects.requireNonNull(SurvivalRPG.getInstance().getCommand("delwarp")).setExecutor(new delwarp());
+        Objects.requireNonNull(SurvivalRPG.getInstance().getCommand("warp")).setExecutor(new warp());
+        Objects.requireNonNull(SurvivalRPG.getInstance().getCommand("setwarp")).setExecutor(new setwarp());
+    }
+
+    public static void registerEvents() {
+        PluginManager pm = SurvivalRPG.getInstance().getServer().getPluginManager();
+        pm.registerEvents(new FlyFeather(SurvivalRPG.getInstance()),SurvivalRPG.getInstance());
+        pm.registerEvents(new TeleportBow(), SurvivalRPG.getInstance());
+    }
+
+    public static void registerMenus() {
+        PluginManager pm = SurvivalRPG.getInstance().getServer().getPluginManager();
+        pm.registerEvents(new PrimaryMenu(), SurvivalRPG.getInstance());
+        pm.registerEvents(new HelpMenu(), SurvivalRPG.getInstance());
+    }
+}
