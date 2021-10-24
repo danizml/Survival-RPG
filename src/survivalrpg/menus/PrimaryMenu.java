d8b2db3228f3a8cf505d0a19327869586720a45a
danizml <josedanielmorebolopez@gmail.com>
2021-10-25 00:58:02 +0200

    Update: PrimaryMenu.java

--- src/survivalrpg/menus/PrimaryMenu.java
+++ src/survivalrpg/menus/PrimaryMenu.java
@@ -4,30 +4,79 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-public class PrimaryMenu {
+/**
+ *    +PrimaryMenu+
+ * File created by TonimatasMC
+ * Links:
+ *  - Discord: https://discord.com/invite/VYqEtT36U9
+ *  - GitHub: https://github.com/TonimatasMCDEV
+ *  - SpigotMC: https://www.spigotmc.org/members/tonimatas.803111/
+ *  - CurseForge: https://www.curseforge.com/members/x_tonimatasmc_x/projects
+ *
+ */
+
+@SuppressWarnings("CommentedOutCode")
+public class PrimaryMenu implements Listener {
     public static Inventory primary;
 
-    public void CreateGuiPrimary() {
-        primary.setItem(0, new ItemStack(Material.WHITE_STAINED_GLASS_PANE));
-        primary.setItem(1, new ItemStack(Material.WHITE_STAINED_GLASS_PANE));
-        primary.setItem(2, new ItemStack(Material.WHITE_STAINED_GLASS_PANE));
-        primary.setItem(3, new ItemStack(Material.WHITE_STAINED_GLASS_PANE));
-        primary.setItem(4, new ItemStack(Material.WHITE_STAINED_GLASS_PANE));
-        primary.setItem(5, new ItemStack(Material.WHITE_STAINED_GLASS_PANE));
-        primary.setItem(6, new ItemStack(Material.WHITE_STAINED_GLASS_PANE));
-        primary.setItem(7, new ItemStack(Material.WHITE_STAINED_GLASS_PANE));
+    public static void CreateGuiPrimary() {
 
-        primary.setItem(8, new ItemStack(Material.BARRIER));
+//----------------------Panels----------------------//
 
-        primary.setItem(9, new ItemStack(Material.WHITE_STAINED_GLASS_PANE));
+        PrimaryMenu.glassPane(0);
+        PrimaryMenu.glassPane(1);
+        PrimaryMenu.glassPane(2);
+        PrimaryMenu.glassPane(3);
+        PrimaryMenu.glassPane(4);
+        PrimaryMenu.glassPane(5);
+        PrimaryMenu.glassPane(6);
+        PrimaryMenu.glassPane(7);
+        PrimaryMenu.glassPane(8);
+        PrimaryMenu.glassPane(9);
+        PrimaryMenu.glassPane(11);
+        PrimaryMenu.glassPane(17);
+        PrimaryMenu.glassPane(18);
+        PrimaryMenu.glassPane(20);
+        PrimaryMenu.glassPane(26);
+        PrimaryMenu.glassPane(27);
+        PrimaryMenu.glassPane(29);
+        PrimaryMenu.glassPane(35);
+        PrimaryMenu.glassPane(36);
+        PrimaryMenu.glassPane(38);
+        PrimaryMenu.glassPane(44);
+        PrimaryMenu.glassPane(45);
+        PrimaryMenu.glassPane(46);
+        PrimaryMenu.glassPane(47);
+        PrimaryMenu.glassPane(48);
+        PrimaryMenu.glassPane(49);
+        PrimaryMenu.glassPane(50);
+        PrimaryMenu.glassPane(51);
+        PrimaryMenu.glassPane(52);
+
+//---------------------Function---------------------//
+
+        ItemStack icon8 = new ItemStack(Material.BARRIER);
+        ItemMeta meta8 = icon8.getItemMeta();
+        assert meta8 != null;
+        meta8.setDisplayName(ChatColor.translateAlternateColorCodes('&', "&dClose menu"));
+        List<String> lore8 = new ArrayList<>();
+        lore8.add(ChatColor.translateAlternateColorCodes('&', "&5Click for close the menu."));
+        meta8.setLore(lore8);
+        icon8.setItemMeta(meta8);
+        primary.setItem(53, icon8);
 
         ItemStack icon10 = new ItemStack(Material.NETHER_STAR);
         ItemMeta meta10 = icon10.getItemMeta();
@@ -39,16 +88,11 @@ public class PrimaryMenu {
         icon10.setItemMeta(meta10);
         primary.setItem(10, icon10);
 
-        primary.setItem(11, new ItemStack(Material.WHITE_STAINED_GLASS_PANE));
-
         /*primary.setItem(12, new ItemStack(Material.BOOK));
         primary.setItem(13, new ItemStack(Material.BOOK));
         primary.setItem(14, new ItemStack(Material.BOOK));
         primary.setItem(15, new ItemStack(Material.BOOK));
         primary.setItem(16, new ItemStack(Material.BOOK));*/
-
-        primary.setItem(17, new ItemStack(Material.WHITE_STAINED_GLASS_PANE));
-        primary.setItem(18, new ItemStack(Material.WHITE_STAINED_GLASS_PANE));
 
         ItemStack icon19 = new ItemStack(Material.COMMAND_BLOCK);
         ItemMeta meta19 = icon19.getItemMeta();
@@ -62,16 +106,11 @@ public class PrimaryMenu {
         icon19.setItemMeta(meta19);
         primary.setItem(19, icon19);
 
-        primary.setItem(20, new ItemStack(Material.WHITE_STAINED_GLASS_PANE));
-
         /*primary.setItem(21, new ItemStack(Material.BOOK));
         primary.setItem(22, new ItemStack(Material.BOOK));
         primary.setItem(23, new ItemStack(Material.BOOK));
         primary.setItem(24, new ItemStack(Material.BOOK));
         primary.setItem(25, new ItemStack(Material.BOOK));*/
-
-        primary.setItem(26, new ItemStack(Material.WHITE_STAINED_GLASS_PANE));
-        primary.setItem(27, new ItemStack(Material.WHITE_STAINED_GLASS_PANE));
 
         ItemStack icon28 = new ItemStack(Material.COMMAND_BLOCK);
         ItemMeta meta28 = icon28.getItemMeta();
