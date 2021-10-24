b6adef054ed1d599629331d7a1a87bf44bf39b25
danizml <josedanielmorebolopez@gmail.com>
2021-10-25 00:59:31 +0200

    Changes by TonimatasMC

--- src/survivalrpg/events/items/TeleportBow.java
+++ src/survivalrpg/events/items/TeleportBow.java
@@ -18,18 +18,12 @@ public class TeleportBow implements Listener {
     @EventHandler
     public void onBowShoot(ProjectileHitEvent e) {
         if (e.getEntity() instanceof Arrow ) {
-            Player playershoot = (Player) e.getEntity().getShooter();
-            int pitch = (int) Objects.requireNonNull(playershoot).getLocation().getPitch();
-            int yaw = (int) playershoot.getLocation().getYaw();
-            int x = e.getEntity().getLocation().getBlockX();
-            int y = e.getEntity().getLocation().getBlockY();
-            int z = e.getEntity().getLocation().getBlockZ();
-            Location location = new Location(playershoot.getWorld(), x, y, z, yaw, pitch);
-            if (Objects.requireNonNull(playershoot.getItemInHand().getItemMeta()).getDisplayName().equals(ChatColor.DARK_PURPLE + "TP_Bow")) {
-                playershoot.teleport(location);
+            Location location = new Location(((Player) Objects.requireNonNull(e.getEntity().getShooter())).getWorld(), e.getEntity().getLocation().getBlockX(), e.getEntity().getLocation().getBlockY(), e.getEntity().getLocation().getBlockZ(), (int) ((Player) e.getEntity().getShooter()).getLocation().getYaw(), (int) Objects.requireNonNull(((Player) e.getEntity().getShooter())).getLocation().getPitch());
+            if (Objects.requireNonNull(((Player) e.getEntity().getShooter()).getItemInHand().getItemMeta()).getDisplayName().equals(ChatColor.DARK_PURPLE + "TP-Bow")) {
+                ((Player) e.getEntity().getShooter()).teleport(location);
                 e.getEntity().remove();
-                playershoot.spawnParticle(Particle.ENCHANTMENT_TABLE, 10, 10, 10, 10);
-                playershoot.playSound(location, Sound.ENTITY_ENDER_PEARL_THROW, 20, 20);
+                ((Player) e.getEntity().getShooter()).spawnParticle(Particle.ENCHANTMENT_TABLE, 10, 10, 10, 10);
+                ((Player) e.getEntity().getShooter()).playSound(location, Sound.ENTITY_ENDER_PEARL_THROW, 20, 20);
             }
         }
     }
