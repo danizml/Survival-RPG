c6252a3beed0b00b4f345d4a3d11fc3c8f10be55
danizml <josedanielmorebolopez@gmail.com>
2021-10-31 14:00:57 +0100

    file:

--- src/survivalrpg/manager/UnregisterManager.java
+++ src/survivalrpg/manager/UnregisterManager.java
@@ -2,17 +2,6 @@ package survivalrpg.manager;
 
 import survivalrpg.SurvivalRPG;
 
-/**
- *    +UnregisterManager+
- * File created by TonimatasMC
- * Links:
- *  - Discord: https://discord.com/invite/VYqEtT36U9
- *  - GitHub: https://github.com/TonimatasMCDEV
- *  - SpigotMC: https://www.spigotmc.org/members/tonimatas.803111/
- *  - CurseForge: https://www.curseforge.com/members/x_tonimatasmc_x/projects
- *
- */
-
 public class UnregisterManager {
     public static void unregister() {
         unregisterYML();
